242a9364cf59db119d40c21e94a5cf260a4465eb
Natukunda Pamela <78596562+Pamella014@users.noreply.github.com>
2026-02-09 18:20:55 +0300

    Update(uganda): Added content to the travel pages

--- pycon2026/urls.py
+++ pycon2026/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('travel/', view=views.traveladvice, name='traveladvice'),
     path('visa/apply/', view=views.visa_apply, name='applying_for_visa'),
     path('visa/letter/', view=views.visa_letter, name='visa_letter'),
+    path('visa/bus/', view=views.visa_bus, name='visa_bus'),
+    path('visa/flying/', view=views.visa_flying, name='visa_flying'),
     # path('fin-aid/', view=views.fin_aid, name='fin_aid'),
     path('team/', view=views.team, name='team'),
     path('report/', view=views.report, name='report'),
